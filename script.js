1366af0ee04b746a9b7bfa4a733ed47e71f61f9e
Umar Arshad <umararshad1995@gmail.com>
2020-07-08 00:04:43 -0400

    new questions

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ var questions = [{
     question: "1. How many NBA Championships did Kobe Bryant win?",
     choices: ["Zero", "Four", "Five", "Nine"],
     correctAnswer: 3
-}, 
+}, {
+    question: "2. Who is the commissioner of NBA?",
+    choices: ["Benjamin", "Adam Silver", "Fab Five", "Quavo"],
+    correctAnswer: 2
+}, {
+    question: "3. What team does LeBron James play for?",
+    choices: ["NY Knicks", "NJ Nets", "LA Lakers", "KC Chiefs"],
+    correctAnswer: 3
+}, {
+    question: "4. In what city is the NBA Bubble taking place?",
+    choices: ["Florida", "Boston", "Dallas", "Orlando"],
+    correctAnswer: 4
+}. {
+    question: "5. Which of these players is nicknamed Linsanity?",
+    choices: ["Dwight Howard", "Carmelo Anthony", "Jeremy Lin", "Jordan Clarkson"],
+    correctAnswer: 3
+}
